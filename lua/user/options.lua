f923436c033e3bcefd251c6504fbb08bfdba4655
bbastanza <b.bastanza@gmail.com>
2022-06-25 09:59:10 -0400

    add $ to iskeyword * simplify whichkey keybindings

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -42,6 +42,10 @@ for k, v in pairs(options) do
 	vim.opt[k] = v
 end
 
---vim.cmd "set whichwrap+=<,>,[,],h,l"
-vim.cmd([[set iskeyword+=-]])
-vim.cmd([[set formatoptions-=cro]]) -- TODO: this doesn't seem to work
+vim.cmd("set whichwrap+=<,>,[,],h,l")
+
+-- dollar sign and dash treated as part of a word
+vim.cmd([[
+set iskeyword+=-
+set iskeyword+=$
+]])
